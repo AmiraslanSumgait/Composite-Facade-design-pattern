88d7878fdbed0aaba36f49b0cf8b7a40e943da42
AmiraslanSumgait <emiraslaneliyev45@gmail.com>
2021-10-19 23:42:22 +0400

    initial

---
+++ Facade and Composite/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Facade_and_Composite
+{
+
+    interface IGraphic
+    {
+        IGraphic Move(int x, int y);
+        IGraphic Draw();
+    }
+
+    class Dot : IGraphic
+    {
+
+        private int _x { get; set; }
+        private int _y { get; set; }
+
+        public Dot(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        virtual public IGraphic Draw()
+        {
+            Console.WriteLine("Dot  drawed");
+            return this;
+        }
+
+        virtual public IGraphic Move(int x, int y)
+        {
+            _x = x;
+            _y = y;
+            return this;
+        }
+    }
+
+    class Circle : Dot
+    {
+        public double Radius { get; set; }
+        public Circle(int x, int y, int Radius) : base(x, y)
+        {
+            this.Radius = Radius;
+        }
+        public override IGraphic Draw()
+        {
+            Console.WriteLine($"Circle drawed");
+            return this;
+        }
+    }
+
+    class CompoundGraphic : IGraphic
+    {
+        public List<IGraphic> childrens = new List<IGraphic>();
+
+        public IGraphic Add(IGraphic child)
+        {
+            childrens.Add(child);
+            return this;
+        }
+        public IGraphic Remove(IGraphic child)
+        {
+            childrens.Remove(child);
+            return this;
+        }
+        public IGraphic Move(int x, int y)
+        {
+            foreach (var children in childrens) children.Move(x, y);
+            return this;
+        }
+        public IGraphic Draw()
+        {
+            foreach (var children in childrens) children.Draw();
+            return this;
+        }
+
+
+    }
+
+
+    class ImageEditor
+    {
+        public CompoundGraphic compoundGraphic { get; set; }
+
+        public void Load()
+        {
+            compoundGraphic = new CompoundGraphic();
+            compoundGraphic.Add(new Circle(10,9,14));
+            compoundGraphic.Add(new Dot(1, 2));
+        }
+
+
+        public void GroupSelected(List<IGraphic> graphics)
+        {
+            var group = new CompoundGraphic();
+            for (int i = 0; i < graphics.Count; i++)
+            {
+                group.Add(graphics[i]);
+                compoundGraphic.Remove(graphics[i]);
+            }
+            compoundGraphic.Add(group);
+            compoundGraphic.Draw();
+        }
+
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ImageEditor image = new ImageEditor();
+            image.Load();
+
+            image.GroupSelected(image.compoundGraphic.childrens);
+        }
+    }
+}
---
+++ Facade/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Facade
+{
+    class Authorize
+    {
+        public void CheckUser()
+        {
+            Console.WriteLine("Check USser");
+        }
+    }
+
+    class Caching
+    {
+        public void Cache()
+        {
+            Console.WriteLine("Cache");
+        }
+    }
+
+    class Logging
+    {
+        public void Log()
+        {
+            Console.WriteLine("Log");
+        }
+    }
+
+    class Validation
+    {
+        public void Validate()
+        {
+            Console.WriteLine("Validate");
+        }
+    }
+
+    class CrossCuttingConcerns
+    {
+        public Authorize Authorize { get; set; }
+        public Caching Caching { get; set; }
+        public Logging Logging { get; set; }
+        public Validation Validation { get; set; }
+
+        public CrossCuttingConcerns(Authorize authorize, Caching caching, Logging logging, Validation validation)
+        {
+            Authorize = authorize;
+            Caching = caching;
+            Logging = logging;
+            Validation = validation;
+        }
+
+        public void UseAll()
+        {
+            Authorize.CheckUser();
+            Caching.Cache();
+            Logging.Log();
+            Validation.Validate();
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Do something");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Authorize authorize = new Authorize();
+            Caching caching = new Caching();
+            Logging logging = new Logging();
+            Validation validate = new Validation();
+            CrossCuttingConcerns crossCuttingConcerns = new CrossCuttingConcerns(authorize, caching, logging, validate);
+            crossCuttingConcerns.UseAll();
+        }
+    }
+}
